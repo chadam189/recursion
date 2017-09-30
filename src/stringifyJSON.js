77382837d5c509d3836422dd228d0655dac2ce7a
Adam Miller <chadam189@gmail.com>
2017-09-30 12:55:26 -0500

    Pass all tests for stringifyJSONspec.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,38 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var result = '';
+
+  if (obj instanceof Function) {
+  	return result;
+  }
+
+  if (Array.isArray(obj)) {
+  	result += '[';
+  	obj.forEach(function (value) {
+  		result += stringifyJSON(value) + ',';
+  	});
+  	if (result.charAt(result.length -1) === ',') {
+  		result = result.slice(0,result.length - 1);
+  	}
+  	return result + ']';
+
+  } else if (typeof obj === 'object' && obj !== null && typeof obj !== 'boolean') {
+  	result += '{';
+  	for (var key in obj) {
+  		if (obj[key] !== undefined && !(obj[key] instanceof Function)) {
+  			result += '\"' + key + '\":' + stringifyJSON(obj[key]) + ',';
+  		}
+  	}
+  	if (result.charAt(result.length -1) === ',') {
+  		result = result.slice(0,result.length - 1);
+  	}
+  	return result + '}';
+  }
+
+  if (typeof obj === 'string') {
+  	return result + '\"' + obj + '\"';
+  } else {
+  	return result + obj;
+  }
 };
